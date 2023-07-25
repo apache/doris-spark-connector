b857fa78c773deafda1159bce4fd4656bc950fc2
gnehil <adamlee489@gmail.com>
2023-07-25 10:26:50 +0800

    json uses array mode by default to be compatible with old versions

--- spark-doris-connector/src/main/java/org/apache/doris/spark/load/DorisStreamLoad.java
+++ spark-doris-connector/src/main/java/org/apache/doris/spark/load/DorisStreamLoad.java
@@ -109,8 +109,8 @@ public class DorisStreamLoad implements Serializable {
             FIELD_DELIMITER = EscapeHandler.escapeString(streamLoadProp.getOrDefault("column_separator", "\t"));
         } else if ("json".equalsIgnoreCase(fileType)) {
             readJsonByLine = Boolean.parseBoolean(streamLoadProp.getOrDefault("read_json_by_line", "false"));
-            boolean stripOuterArray = Boolean.parseBoolean(streamLoadProp.getOrDefault("strip_outer_array", "false"));
-            if (readJsonByLine == stripOuterArray) {
+            boolean stripOuterArray = Boolean.parseBoolean(streamLoadProp.getOrDefault("strip_outer_array", "true"));
+            if (readJsonByLine && stripOuterArray) {
                 throw new IllegalArgumentException("Only one of options 'read_json_by_line' and 'strip_outer_array' can be set to true");
             }
         }
