283f930226f81bbe2b90a2600335c278cff4d421
lichuang <JasonLee1781@163.com>
2023-09-14 20:45:52 +0800

    [bug] NPE occurs for StringType when column value is null.

--- spark-doris-connector/src/main/scala/org/apache/doris/spark/sql/SchemaUtils.scala
+++ spark-doris-connector/src/main/scala/org/apache/doris/spark/sql/SchemaUtils.scala
@@ -156,7 +156,7 @@ private[spark] object SchemaUtils {
       case LongType => row.getLong(ordinal)
       case FloatType => row.getFloat(ordinal)
       case DoubleType => row.getDouble(ordinal)
-      case StringType => row.getUTF8String(ordinal).toString
+      case StringType => Option(row.getUTF8String(ordinal)).map(_.toString).getOrElse(DataUtil.NULL_VALUE)
       case TimestampType =>
         LocalDateTime.ofEpochSecond(row.getLong(ordinal) / 100000, (row.getLong(ordinal) % 1000).toInt, ZoneOffset.UTC)
         new Timestamp(row.getLong(ordinal) / 1000).toString
