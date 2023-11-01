4f2ef8df05b0860ab37acd6c2997d209ed4e3b3d
gnehil <adamlee489@gmail.com>
2023-11-01 17:39:57 +0800

    [fix] complex type npe problem (#151)

--- spark-doris-connector/src/test/scala/org/apache/doris/spark/sql/TestConnectorWriteDoris.scala
+++ spark-doris-connector/src/test/scala/org/apache/doris/spark/sql/TestConnectorWriteDoris.scala
@@ -17,7 +17,8 @@
 
 package org.apache.doris.spark.sql
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{ArrayType, IntegerType, MapType, StringType, StructField, StructType}
 import org.junit.{Ignore, Test}
 
 // This test need real connect info to run.
@@ -125,8 +126,61 @@ class TestConnectorWriteDoris {
       .option("sink.batch.size", 2)
       .option("sink.max-retries", 2)
       .option("sink.properties.format", "json")
-      // .option("sink.properties.read_json_by_line", "true")
-      .option("sink.properties.strip_outer_array", "true")
+      .save()
+    spark.stop()
+  }
+
+
+  /**
+   * correct data in doris
+   * +------+--------------+-------------+------------+
+   * | id   | a            | m           | s          |
+   * +------+--------------+-------------+------------+
+   * |    1 | [1, 2, 3]    | {"k1":1}    | {10, "ab"} |
+   * |    2 | [4, 5, 6]    | {"k2":3}    | NULL       |
+   * |    3 | [7, 8, 9]    | NULL        | {20, "cd"} |
+   * |    4 | NULL         | {"k3":5}    | {30, "ef"} |
+   * |    5 | [10, 11, 12] | {"k4":7}    | {40, NULL} |
+   * |    6 | [13, 14, 15] | {"k5":NULL} | {50, NULL} |
+   * |    7 | []           | {}          | {60, "gh"} |
+   * +------+--------------+-------------+------------+
+   */
+  @Test
+  def complexWriteTest(): Unit = {
+    val spark = SparkSession.builder().master("local[1]").getOrCreate()
+
+    val data = Array(
+      Row(1, Array(1,2,3), Map("k1" -> 1), Row(10, "ab")),
+      Row(2, Array(4,5,6), Map("k2" -> 3), null),
+      Row(3, Array(7,8,9), null, Row(20, "cd")),
+      Row(4, null, Map("k3" -> 5), Row(30, "ef")),
+      Row(5, Array(10,11,12), Map("k4" -> 7), Row(40, null)),
+      Row(6, Array(13,14,15), Map("k5" -> null), Row(50, "{10, \"ab\"}")),
+      Row(7, Array(), Map(), Row(60, "gh"))
+    )
+
+    val schema = StructType(
+      Array(
+        StructField("id", IntegerType),
+        StructField("a", ArrayType(IntegerType)),
+        StructField("m", MapType(StringType, IntegerType)),
+        StructField("s", StructType(Seq(StructField("a", IntegerType),StructField("b", StringType))))
+      )
+    )
+
+    val rdd = spark.sparkContext.parallelize(data)
+    val df = spark.createDataFrame(rdd, schema)
+    df.printSchema()
+    df.show(false)
+    df.write
+      .format("doris")
+      .option("doris.fenodes", dorisFeNodes)
+      .option("doris.table.identifier", dorisTable)
+      .option("user", dorisUser)
+      .option("password", dorisPwd)
+      .option("sink.batch.size", 2)
+      .option("sink.max-retries", 0)
+      .option("sink.properties.format", "json")
       .save()
     spark.stop()
   }
