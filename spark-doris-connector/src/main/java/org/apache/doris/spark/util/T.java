efa72824bd29ee49af912409179924bd54f5b6cf
范海乐(10034466) <fanhl-a@glodon.com>
2024-01-15 18:24:51 +0800

    gz

--- spark-doris-connector/src/main/java/org/apache/doris/spark/util/T.java
+++ spark-doris-connector/src/main/java/org/apache/doris/spark/util/T.java
@@ -2,7 +2,7 @@ package org.apache.doris.spark.util;
 
 public class T {
     public static void main(String[] args) {
-        System.out.println("1111111111");
+        System.out.println("11111111111");
         System.out.println("Gz".toLowerCase());
     }
 }
