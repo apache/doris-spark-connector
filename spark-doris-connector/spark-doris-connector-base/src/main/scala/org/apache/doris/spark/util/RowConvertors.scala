e821f0513ccdc61b94ce2c3ed312afe8900cf893
gnehil <adamlee489@gmail.com>
2025-01-06 18:18:51 +0800

    fix read date type convert issue

--- spark-doris-connector/spark-doris-connector-base/src/main/scala/org/apache/doris/spark/util/RowConvertors.scala
+++ spark-doris-connector/spark-doris-connector-base/src/main/scala/org/apache/doris/spark/util/RowConvertors.scala
@@ -109,7 +109,7 @@ object RowConvertors {
     dataType match {
       case StringType => UTF8String.fromString(v.asInstanceOf[String])
       case TimestampType => DateTimeUtils.fromJavaTimestamp(Timestamp.valueOf(v.asInstanceOf[String]))
-      case DateType => DateTimeUtils.fromJavaDate(Date.valueOf(v.asInstanceOf[String]))
+      case DateType => DateTimeUtils.fromJavaDate(v.asInstanceOf[Date])
       case _: MapType =>
         val map = v.asInstanceOf[Map[String, String]]
         val keys = map.keys.toArray
