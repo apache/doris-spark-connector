3c3df396ee9f3af62d5817cc3f2381f25db3c0a0
wudi <676366545@qq.com>
2025-01-23 11:31:19 +0800

    [Improvement] add resposne ignore field config (#260)

--- spark-doris-connector/spark-doris-connector-base/src/main/java/org/apache/doris/spark/rest/models/Schema.java
+++ spark-doris-connector/spark-doris-connector-base/src/main/java/org/apache/doris/spark/rest/models/Schema.java
@@ -17,10 +17,13 @@
 
 package org.apache.doris.spark.rest.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Schema {
 
     private int status = 0;
