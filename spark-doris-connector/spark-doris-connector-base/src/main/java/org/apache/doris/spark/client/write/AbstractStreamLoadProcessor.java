edad07ab6a5c0eb8c940c02dce36572d1078ed2e
LeiWang <leihz1989@gmail.com>
2025-03-24 18:11:33 +0800

    add stream load entity to enhance performance

--- spark-doris-connector/spark-doris-connector-base/src/main/java/org/apache/doris/spark/client/write/AbstractStreamLoadProcessor.java
+++ spark-doris-connector/spark-doris-connector-base/src/main/java/org/apache/doris/spark/client/write/AbstractStreamLoadProcessor.java
@@ -162,6 +162,7 @@ public abstract class AbstractStreamLoadProcessor<R> extends DorisWriter<R> impl
             output.write(lineDelimiter);
         }
         output.write(toFormat(row, format));
+        currentBatchCount++;
     }
 
     @Override
