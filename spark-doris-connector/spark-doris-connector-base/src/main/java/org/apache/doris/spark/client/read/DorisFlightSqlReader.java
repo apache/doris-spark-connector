cdff147ff2df7e0ae833a6664826ed7e1889ab53
LeiWang <leihz1989@gmail.com>
2025-05-23 09:49:48 +0800

    1. add option DORIS_READ_FLIGHT_SQL_PREFIX to spec flight sql prefix 2. add fe retry and random strategy when initialize adbc connection  to achieve load balancing for FE

--- spark-doris-connector/spark-doris-connector-base/src/main/java/org/apache/doris/spark/client/read/DorisFlightSqlReader.java
+++ spark-doris-connector/spark-doris-connector-base/src/main/java/org/apache/doris/spark/client/read/DorisFlightSqlReader.java
@@ -44,7 +44,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Collections;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 import java.util.stream.Collectors;
