80dde655533066f8099568e5ca331eb146455810
wangguoxing <wangguoxing@kingsoft.com>
2025-09-25 16:30:36 +0800

    fix COALESCE itcase error

--- spark-doris-connector/spark-doris-connector-it/src/test/resources/container/ddl/read_filter_pushdown.sql
+++ spark-doris-connector/spark-doris-connector-it/src/test/resources/container/ddl/read_filter_pushdown.sql
@@ -8,12 +8,12 @@ CREATE TABLE tbl_expression_notpushdown (
                                       A2 varchar(300) NULL,
                                       A3 varchar(300) NULL,
                                       A4 varchar(300) NULL,
-                                      __source_ts_ms bigint NULL COMMENT "数据采入kafka的时间戳（集成任务自建）",
-                                      __op varchar(10) NULL COMMENT "操作类型 c:增加 u:更新 d:删除（集成任务自建）r:稽核后补采",
-                                      __table varchar(100) NULL COMMENT "源表表名（集成任务自建）",
-                                      __db varchar(50) NULL COMMENT "源库名称（集成任务自建）",
-                                      __deleted varchar(10) NULL COMMENT "删除标志：false代表有效，true代表已删除（集成任务自建）",
-                                      __dt datetime NULL COMMENT "入库时间，分区字段（集成任务自建）"
+                                      __source_ts_ms bigint NULL,
+                                      __op varchar(10) NULL,
+                                      __table varchar(100) NULL,
+                                      __db varchar(50) NULL,
+                                      __deleted varchar(10) NULL,
+                                      __dt datetime NULL
 ) ENGINE=OLAP
 DUPLICATE KEY(ID)
 DISTRIBUTED BY HASH(`ID`) BUCKETS 2
