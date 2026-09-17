b94df1abd9ed5d5dec73174bd5015f97ee926608
wudi <676366545@qq.com>
2026-09-17 14:06:02 +0800

    [fix] Use Quay MinIO image in S3 TVF tests

--- spark-doris-connector/spark-doris-connector-it/src/test/java/org/apache/doris/spark/container/AbstractS3TvfTestBase.java
+++ spark-doris-connector/spark-doris-connector-it/src/test/java/org/apache/doris/spark/container/AbstractS3TvfTestBase.java
@@ -52,7 +52,7 @@ public abstract class AbstractS3TvfTestBase extends AbstractContainerTestBase {
 
     private static final int MINIO_PORT = 9000;
     private static final String MINIO_IMAGE =
-            "minio/minio:RELEASE.2024-10-13T13-34-11Z";
+            "quay.io/minio/minio:RELEASE.2024-10-13T13-34-11Z";
 
     private static GenericContainer<?> minio;
     private static S3Client s3Client;
